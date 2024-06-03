3d5da0acf20c9fcb4a37be1f5af07994b4ebfa82
Andrew Furey <andrewfurey2003@gmail.com>
2024-06-03 11:01:03 +0100

    test

--- main.go
+++ main.go
@@ -9,7 +9,15 @@ func main() {
 	var code uint16 = 0b0110111001010001
 	fmt.Println("Incorrect Codeword:", strconv.FormatUint(uint64(code), 2))
 
-	fmt.Println("Error should be 9:", check_codeword(code))
+	code = correct_codeword(code)
+	fmt.Println("Correct Codeword:  ", strconv.FormatUint(uint64(code), 2))
+
+	fmt.Println("Error should be 0:", check_codeword(code))
+
+	code ^= 1 << 8
+
+	fmt.Println("Error should be 8:", check_codeword(code))
+
 	code = correct_codeword(code)
 	fmt.Println("Correct Codeword:  ", strconv.FormatUint(uint64(code), 2))
 
